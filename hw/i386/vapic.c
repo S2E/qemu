0560c9985dd31b2a0eb7f0521d508600724a5944
Vitaly Chipounov <vitaly@chipounov.fr>
2026-04-03 23:04:08 +0200

    hw/i386: don't abort if vapic patching failed
    Signed-off-by: Vitaly Chipounov <vitaly@chipounov.fr>

--- hw/i386/vapic.c
+++ hw/i386/vapic.c
@@ -439,7 +439,8 @@ static void do_patch_instruction(CPUState *cs, run_on_cpu_data data)
         patch_call(x86_cpu, ip + 1, handlers->get_tpr_stack);
         break;
     default:
-        abort();
+        fprintf(stderr, "kvmvapic: invalid byte, patching failed\n");
+        break;
     }
 
     g_free(info);
